6707c56350cd834657ae7e4b338c1374c1d2dd66
zacharyb12 <boualemzachary89@yahoo.fr>
2025-09-26 18:03:03 +0200

    exo guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,13 @@ import { BookDetails } from '../components/pages/book-details/book-details';
 import { Homepage } from '../components/pages/homepage/homepage';
 
 export const routes: Routes = [
+    
     {path : '' , component : Homepage},
     {path : 'books' , component : BookList},
+    {path : 'book-details/:id' , component : BookDetails},
+    // limiter l'acces a ces routes aux utilisateurs authentifies
+    // {path : 'book-add' , loadComponent : () => import('route vers le composant').then(c => c.Composant)},
     {path : 'book-add' , component : BookAdd},
     {path : 'book-edit/:id' , component : BookUpdate},
-    {path : 'book-details/:id' , component : BookDetails}
 
 ];
--- src/core/services/auth-service/auth-service.ts
+++ src/core/services/auth-service/auth-service.ts
@@ -1,4 +1,4 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { User } from '../../models/user.model';
 
 @Injectable({
